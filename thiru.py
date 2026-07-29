41fa8e5c65166a6d39dc4991caf95e900b6808df
Thirumalesh A <thirumalesha907@gmail.com>
2026-07-29 22:11:13 +0530

    first digit

--- thiru.py
+++ thiru.py
@@ -1,10 +1,8 @@
 n =  int(input("enter a n value"))
-product = 1
-while n > 0:
-    digit = n %10
-    product = product * digit
+
+while n > 10:
     n = n // 10
-print("product of digits:",product)
+print("first digits:",n)
 
     
 
