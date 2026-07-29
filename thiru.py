578096d77975021419cfc6ba80e3948f4a0f312c
Thirumalesh A <thirumalesha907@gmail.com>
2026-07-29 20:58:02 +0530

    feat: add logic to reverse a number

--- thiru.py
+++ thiru.py
@@ -1,11 +1,14 @@
 n =  int(input("enter a n value"))
-count = 0 
-
-while n> 0:
-    count = count + 1
+rev = 0
+while n>0:
+    digit =n % 10
+    rev = rev * 10 + digit
     n = n // 10
+print("reverse = ",rev)
 
-print("digits =",count)
+    
+
+
 
 
 
