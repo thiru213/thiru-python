f7a8937edd9e33764a4191c287d28213ce29d457
Thirumalesh A <thirumalesha907@gmail.com>
2026-07-29 22:36:56 +0530

    print factors

--- thiru.py
+++ thiru.py
@@ -1,11 +1,7 @@
 n =  int(input("enter a n value"))
-count = 0
-while n >0:
-    digit = n % 10
-    if digit % 2 != 0:
-      count = count + 1
-    n = n // 10
-print("odd digits:",count)
+for i in range(1,n+1):
+  if n % i == 0:
+    print(i)
 
     
 
