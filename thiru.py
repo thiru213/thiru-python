df892914fc24eb8ba62a577dbb6e57e665010bf5
Thirumalesh A <thirumalesha907@gmail.com>
2026-07-30 19:26:19 +0530

    feat: power using loops

--- thiru.py
+++ thiru.py
@@ -1,9 +1,16 @@
-n =  int(input("enter a n value"))
-count = 0
-for i in range(1,n+1):
-  if n % i == 0:
-    count =count+1
-    print(i)
+base =  int(input("enter a base "))
+power =  int(input("enter a power "))
+result = 1
+for i in range (power):
+    result = result*base
+print("Answer +",result)
+
+
+
+
+    
+
+
 
     
 
