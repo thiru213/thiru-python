8e60889346953cdd8434ec672c2cc5584d5bd998
Thirumalesh A <thirumalesha907@gmail.com>
2026-08-03 19:24:46 +0530

    feat : amstrong number

--- thiru.py
+++ thiru.py
@@ -1,10 +1,15 @@
-base =  int(input("enter a base "))
-power =  int(input("enter a power "))
-result = 1
-for i in range (power):
-    result = result*base
-print("Answer +",result)
-
+n = int(input(" enter a number:"))
+temp = n
+digit = len(str(n))
+total = 0
+while temp > 0:
+    d = temp % 10
+    total =  total + d * d * d 
+    temp = temp // 10
+if total == n:
+    print("the number is armstrong")
+else:
+    print("the number is not armstrong")
 
 
 
