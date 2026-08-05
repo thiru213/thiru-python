d1b45e4854f3a0a760971cfc2b5f6aed8e5530b9
Thirumalesh A <thirumalesha907@gmail.com>
2026-08-05 20:06:32 +0530

    feat : palindrome letter

--- thiru.py
+++ thiru.py
@@ -1,18 +1,9 @@
-start = int(input("Enter start: "))
-end = int(input("Enter end: "))
-
-for num in range(start, end + 1):
-    temp = num
-    total = 0
-    digits = len(str(num))
-
-    while temp > 0:
-        digit = temp % 10
-        total = total + digit ** digits
-        temp = temp // 10
-
-    if num == total:
-        print(num)
+word = (input("Enter word: "))
+rev = word[::-1]
+if word == rev:
+        print("The number is a palindrome")
+else:
+     print("The number is not a palindrome")
 
 
     
