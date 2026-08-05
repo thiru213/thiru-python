53c453996f1017b0235ecf2b3039aef767645a9b
Thirumalesh A <thirumalesha907@gmail.com>
2026-08-05 22:12:28 +0530

    feat: Automorphic

--- thiru.py
+++ thiru.py
@@ -1,15 +1,10 @@
 n = int(input("Enter a number: "))
-square = n * n
-temp = square
-sum = 0
-while temp > 0:
-    digit = temp % 10
-    sum = sum + digit
-    temp = temp // 10
-if sum  == n:
-    print( "is a neon  number")
+square = n*n
+
+if str(square).endswith(str(n)):
+    print("is an automorphic number.")
 else:
-    print( "is not a neon number")
+    print("is not an automorphic number.")    
 
 
     
